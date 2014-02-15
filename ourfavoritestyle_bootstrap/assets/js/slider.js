220dc30a136fd81e3c85ba4b5073c5d2ab015181
pepegrillo <josuea3lopez@gmail.com>
2014-02-15 16:33:05 +0000

    Todo el archivo codio actualizado Viernes 14Feb2014

---
+++ ourfavoritestyle_bootstrap/assets/js/slider.js
@@ -0,0 +1,13 @@
+/**
+* Created with Our_Favorite_Style.
+* User: pepegrillo
+* Date: 2014-02-14
+* Time: 03:08 PM
+* To change this template use Tools | Templates.
+*/
+// Can also be used with $(document).ready()
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+});
